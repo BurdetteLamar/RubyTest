0c27d68554e5ae5ed01fdd6957f5625f9abb11c7
Burdette Lamar <BurdetteLamar@Yahoo.com>
2017-04-21 16:51:41 -0500

    Added verdicts to Albums test.

--- examples/rest_api/tests/albums_test.rb
+++ examples/rest_api/tests/albums_test.rb
@@ -2,13 +2,23 @@ require_relative 'base_class_for_test'
 
 require_relative '../endpoints/albums/get_albums'
 
+require_relative '../../../lib/log'
+
 class AlbumsTest < BaseClassForTest
 
   def test_get_albums
 
     prelude do |client, log|
 
-      p GetAlbums.call(client)
+      albums = GetAlbums.call(client)
+      album = albums.first
+      if log.verdict_assert_instance_of?('album class', Album, album, 'First object is an Album')
+        log.verdict_assert_operator?('positive id', 0, :<, album.id, 'Album id is positive')
+        log.verdict_assert_operator?('positive user-id', 0, :<, album.id, 'Album user-id is positive')
+        if log.verdict_assert_instance_of?('title class', String, album.title, 'Album title is a String')
+          log.verdict_refute_empty?('non-empty title', album.title, 'Album title is not empty')
+        end
+      end
 
     end
 
