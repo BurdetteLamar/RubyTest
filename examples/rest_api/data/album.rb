fa23687a270c4b0d17e65991b67159b3a80743d6
Burdette Lamar <BurdetteLamar@Yahoo.com>
2017-04-21 17:30:30 -0500

    Move data validation from test to data object (Album).

--- examples/rest_api/data/album.rb
+++ examples/rest_api/data/album.rb
@@ -16,4 +16,15 @@ class Album < BaseClassForData
     super(FIELDS, values)
   end
 
+  Contract Log, String => Bool
+  def verdict_valid?(log, verdict_id)
+    if log.verdict_assert_instance_of?(verdict_id + ' - class', Album, self, 'First object is an Album')
+      log.verdict_assert_operator?(verdict_id + ' - positive id', 0, :<, self.id, 'Album id is positive')
+      log.verdict_assert_operator?(verdict_id + ' - positive user-id', 0, :<, self.id, 'Album user-id is positive')
+      if log.verdict_assert_instance_of?(verdict_id + ' - title class', String, self.title, 'Album title is a String')
+        log.verdict_refute_empty?(verdict_id + ' - non-empty title', self.title, 'Album title is not empty')
+      end
+    end
+  end
+
 end
