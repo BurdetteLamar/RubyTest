5fdaed80addb63537e74a0c517e56c09b51a2174
Burdette Lamar <burdettelamar@yahoo.com>
2017-05-21 12:06:47 -0500

    More on Changes Report.

--- lib/changes_report.rb
+++ lib/changes_report.rb
@@ -7,6 +7,15 @@ require_relative '../lib/helpers/test_helper'
 
 class ChangesReport < BaseClass
 
+  class VerdictPair
+
+    attr_accessor :prev, :curr, :status
+    def initialize(prev_verdict, curr_verdict)
+
+    end
+
+  end
+
   Contract String, String => nil
   def self.create_report(app_name, report_file_path)
 
@@ -25,38 +34,88 @@ class ChangesReport < BaseClass
         'verdict_paths.txt',
     )
     FileUtils.touch(verdict_paths_file_path) unless File.exist?(verdict_paths_file_path)
-    verdict_paths = []
+    lines = nil
     File.open(verdict_paths_file_path, 'r') do |file|
       lines = file.readlines
-      verdict_paths = Set.new(lines.delete_if{|line| line.start_with?('#')})
-      verdict_paths = Set.new(lines.delete_if{|line| line.gsub(/\s/, '').empty?})
-      verdict_paths.collect!{|line| line.chomp }
-      verdict_paths.merge(prev_verdicts.keys)
-      verdict_paths.merge(curr_verdicts.keys)
     end
-    updated_verdict_paths = <<EOT
+
+    lines = lines.delete_if{|line| line.start_with?('#')}
+    lines = lines.delete_if{|line| line.gsub(/\s/, '').empty?}
+    expected_verdict_paths = lines.collect{|line| line.chomp }.to_set
+
+    prev_verdict_paths = Set.new(prev_verdicts.keys)
+    curr_verdict_paths = Set.new(curr_verdicts.keys)
+    updated_verdict_paths = Set.new(expected_verdict_paths).merge(curr_verdict_paths)
+
+    updated_verdict_paths_text = <<EOT
 # This file is updated automatically when the Changes Report is generated.
 
 # Any new verdict path found in current or previous results is added.
 # No verdict path is ever deleted automatically, so any needed culling
 # (e.g., a verdict path that's not longer in the tests) must be manual.
 
-# Most recently updated from:
-# - #{prev_dir_path}
-# - #{curr_dir_path}
+# Most recently updated""
+# - at #{TimeHelper.timestamp}
+# - from #{curr_dir_path}
 
-#{verdict_paths.to_a.sort.join("\n")}
+#{updated_verdict_paths.to_a.sort.join("\n")}
 
 EOT
     File.open(verdict_paths_file_path, 'w') do |file|
-      file.write(updated_verdict_paths)
+      file.write(updated_verdict_paths_text)
     end
 
-    key_comparison = SetHelper.compare(prev_verdicts.keys.to_set, curr_verdicts.keys.to_set)
     # Key comparison is hash with keys :missing, :unexpected, :ok.
-    # Each :missing is a new blocked verdict.
-    # Each :unexpected is a new passed verdict or a new failed verdict.
-    # Each :ok is a verdict
+    key_comparison = SetHelper.compare(prev_verdicts.keys.to_set, curr_verdicts.keys.to_set)
+    verdict_pairs = {}
+
+    key_comparison.fetch(:missing).each do |verdict_path|
+      prev_verdict = prev_verdicts.fetch(verdict_path)
+      curr_verdict = nil
+      verdict_pair = ChangesReport::VerdictPair.new(prev_verdict, curr_verdict)
+      verdict_pairs.store(verdict_path, verdict_pair)
+    end
+
+    key_comparison.fetch(:unexpected).each do |verdict_path|
+      prev_verdict = nil
+      curr_verdict = curr_verdicts.fetch(verdict_path)
+      verdict_pair = ChangesReport::VerdictPair.new(prev_verdict, curr_verdict)
+      verdict_pairs.store(verdict_path, verdict_pair)
+    end
+
+    key_comparison.fetch(:ok).each do |verdict_path|
+      prev_verdict = prev_verdicts.fetch(verdict_path)
+      curr_verdict = curr_verdicts.fetch(verdict_path)
+      verdict_pair = ChangesReport::VerdictPair.new(prev_verdict, curr_verdict)
+      verdict_pairs.store(verdict_path, verdict_pair)
+    end
+
+    # One more case:  Verdict was in neither prev or curr, but was expected
+    # (i.e., was in verdict_paths.txt).
+    p expected_verdict_paths.class
+    p prev_verdict_paths.class
+    p curr_verdict_paths.class
+    old_blocked_paths = expected_verdict_paths - prev_verdict_paths - curr_verdict_paths
+    old_blocked_paths.each do |verdict_path|
+      verdict_pair = ChangesReport::VerdictPair.new(nil, nil)
+    end
+
+    # Ok:  verdict was in both prev and curr.
+    # Old passed:  both passed with no change.
+    # Old failed:  both failed with no change.
+    # Changed passed:  both passed with change.
+    # Changed failed:  both failed with change.
+    # New failed:  prev passed, curr failed.
+    # New passed:  prev failed, curr passed.
+
+    # Missing:  verdict was in prev, but not in curr.
+    # New blocked.
+
+    # Unexpected:  Verdict was in curr, but not in prev.
+    # New passed.
+    # New failed.
+
+    # Old blocked.
     nil
   end
 
