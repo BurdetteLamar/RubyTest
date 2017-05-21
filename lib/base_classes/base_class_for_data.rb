99fd735a409310a9c35d9b7423ca366e53e35624
Burdette Lamar <burdettelamar@yahoo.com>
2017-05-21 13:34:40 -0500

    Added  self.equal?, cleaned up some other.

--- lib/base_classes/base_class_for_data.rb
+++ lib/base_classes/base_class_for_data.rb
@@ -27,13 +27,24 @@ class BaseClassForData < BaseClass
     nil
   end
 
+  Contract Any, Any => Bool
+  # Compare recursively, so that nested objects are compared.
+  def self.equal?(expected_obj, actual_obj)
+    if expected_obj.kind_of?(self.class)
+      self.equal_recursive?(expected_obj, actual_obj)
+    else
+      actual_obj == expected_obj
+    end
+  end
+
   Contract Log, String, Any, Any, String => Bool
   # Verify recursively, so that nested objects can verify themselves.
   def self.verdict_equal?(log, verdict_id, expected_obj, actual_obj, message)
-    unless expected_obj.respond_to?(:fields)
-      return log.verdict_assert_equal?(verdict_id, expected_obj, actual_obj, message)
+    if expected_obj.kind_of?(self.class)
+      self.verdict_equal_recursive?(log, verdict_id, expected_obj, actual_obj, message)
+    else
+      log.verdict_assert_equal?(verdict_id, expected_obj, actual_obj, message)
     end
-    self.verdict_equal_recursive?(log, verdict_id, expected_obj, actual_obj, message)
   end
 
   Contract None => Hash
@@ -54,7 +65,7 @@ class BaseClassForData < BaseClass
   # giving special handling to nested objects.
   def log_recursive(obj, log, comment = obj.name)
     return if obj.nil?
-    unless obj.respond_to?(:fields)
+    unless obj.kind_of?(self.class)
       # Can't process it below;  just log it here.
       log.put_element('data', obj.to_s)
       return
@@ -83,6 +94,21 @@ class BaseClassForData < BaseClass
     end
   end
 
+  # Compare an object recursively,
+  # giving special handling to nested objects.
+  def self.equal_recursive?(expected_obj, actual_obj)
+    expected_obj.fields do |field|
+      expected_value = expected_obj.send(field)
+      next if expected_value.nil?
+      actual_value = actual_obj.send(field)
+      if actual_obj.kind_of?(self.class)
+        self.equal_recursive?(expected_obj, actual_obj)
+      else
+        actual_value == expected_value
+      end
+    end
+  end
+
   # Verify an object recursively,
   # giving special handling to nested objects.
   def self.verdict_equal_recursive?(log, verdict_id, expected_obj, actual_obj, message)
@@ -93,12 +119,11 @@ class BaseClassForData < BaseClass
         # No expected value?  Skip it.
         next if expected_value.nil?
         actual_value = actual_obj.send(field)
-        case
-          when actual_value.class.respond_to?(:verdict_equal_recursive?)
-            v_id = format('%s %s', verdict_id, field)
-            self.verdict_equal_recursive?(log, v_id, expected_value, actual_value, message)
-          else
-            verdict = log.verdict_assert_equal?('%s-%s' % [verdict_id, field.downcase], expected_value, actual_value, message) && verdict
+        if actual_obj.kind_of?(self.class)
+          v_id = format('%s %s', verdict_id, field)
+          self.verdict_equal_recursive?(log, v_id, expected_value, actual_value, message)
+        else
+          verdict = log.verdict_assert_equal?('%s-%s' % [verdict_id, field.downcase], expected_value, actual_value, message) && verdict
         end
       end
     end
