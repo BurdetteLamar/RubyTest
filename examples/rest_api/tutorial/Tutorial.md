19c5bed5e795d5bcce97b35992f8b606c259841a
Burdette Lamar <BurdetteLamar@Yahoo.com>
2017-09-14 14:15:20 -0500

    Experiment for tutorial

---
+++ examples/rest_api/tutorial/Tutorial.md
@@ -0,0 +1,11 @@
+# REST API Tutorial
+
+## Nothing Test
+
+Code:
+
+![nothing_test.rb](./images/nothing_test.rb.png)
+
+Log:
+
+![test_nothing.xml](./images/test_nothing.xml.png)
---
+++ examples/rest_api/tutorial/tests/nothing_test.rb
@@ -0,0 +1,12 @@
+require_relative '../../base_classes/base_class_for_test'
+
+class MyTest < BaseClassForTest
+
+  def test_nothing
+
+    prelude do |client, log|
+    end
+
+  end
+
+end
