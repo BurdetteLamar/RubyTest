82d12a8a4fbc56b1bca58dd57533448c12b0e66e
Burdette Lamar <BurdetteLamar@Yahoo.com>
2017-09-18 16:03:31 -0500

    Tests for tutorial

--- examples/rest_api/tutorial/tests/nothing_test.rb
+++ examples/rest_api/tutorial/tests/nothing_test.rb
@@ -1,10 +1,13 @@
 require_relative '../../base_classes/base_class_for_test'
 
-class MyTest < BaseClassForTest
+class NothingTest < BaseClassForTest
 
   def test_nothing
 
     prelude do |client, log|
+
+      # Test code goes here.
+
     end
 
   end
