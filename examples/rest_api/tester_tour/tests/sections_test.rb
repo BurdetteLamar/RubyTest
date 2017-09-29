ab941a618ec34772b25c4403ce0c1c35e62b252d
burdettelamar <BurdetteLamar@Yahoo.com>
2017-09-29 14:32:08 -0500

    More on tour

--- examples/rest_api/tester_tour/tests/sections_test.rb
+++ examples/rest_api/tester_tour/tests/sections_test.rb
@@ -6,28 +6,28 @@ class SectionsTest < BaseClassForTest
     prelude do |_, log|
       log.section('First outer section') do
         log.section('First inner section') do
-          log.comment('Some test code goes here')
+          log.comment('Some test code can go here')
         end
         log.section('Second inner section') do
-          log.comment('Some test code goes here')
+          log.comment('Some test code can go here')
         end
       end
       log.section('Second outer section') do
-        log.comment('Some test code goes here')
+        log.comment('Some test code can go here')
       end
       log.section('Section with timestamp', :timestamp) do
-        log.comment('Some test code goes here')
+        log.comment('Some test code can go here')
       end
       log.section('Section with timestamp', :duration) do
-        log.comment('Some test code goes here')
+        log.comment('Some test code can go here')
         sleep 1
       end
       log.section('Section with timestamp and duration', :timestamp, :duration) do
-        log.comment('Some test code goes here')
+        log.comment('Some test code can go here')
         sleep 2
       end
       log.section('Order does not matter', :duration, :timestamp) do
-        log.comment('Some test code goes here')
+        log.comment('Some test code can go here')
         sleep 3
       end
     end
